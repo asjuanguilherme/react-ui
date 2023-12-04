577b92fc2363af5f1c17feaa149551dcc6c9d42f
Juan Guilherme <asjuanguilherme@gmail.com>
2023-12-04 01:35:12 -0300

    feat: :wrench: adjust colors

--- src/styles/tokens/colors.ts
+++ src/styles/tokens/colors.ts
@@ -1,22 +1,32 @@
 export const colors = {
   primary: {
-    dark: '#D15933',
-    normal: '#F47C46',
-    light: '#F8A573',
+    light: '#E7D9FF',
+    normal: '#9F4DFF',
+    dark: '#5D25B2',
+  },
+  secondary: {
+    light: '#AED4E6',
+    normal: '#5E99B2',
+    dark: '#3A6078',
   },
   error: {
-    light: '#fca5a5',
-    normal: '#ef4444',
-    dark: '#b91c1c',
+    light: '#FFD8D8',
+    normal: '#FF4C4C',
+    dark: '#B21818',
   },
   success: {
-    light: '#86efac',
-    normal: '#22c55e',
-    dark: '#15803d',
+    light: '#A4FFC2',
+    normal: '#00A860',
+    dark: '#006F3E',
   },
   warning: {
-    dark: '#A08320',
-    normal: '#bb9e2d',
-    light: '#D6BF5C',
+    light: '#FFE194',
+    normal: '#FF9E00',
+    dark: '#CC7000',
+  },
+  info: {
+    light: '#CCE5FF',
+    normal: '#3399FF',
+    dark: '#1A66B2',
   },
 }
