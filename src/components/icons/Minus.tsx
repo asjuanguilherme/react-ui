0dd343c7babbc262174e1f207995ef79e999aa9e
Juan Guilherme <asjuanguilherme@gmail.com>
2023-12-04 03:20:23 -0300

    feat: :sparkles: add new icons

---
+++ src/components/icons/Minus.tsx
@@ -0,0 +1,7 @@
+import { createIconComponent } from './createIconComponent'
+export const MinusIcon = createIconComponent({
+  content: (
+    <path d="M432 256C432 278.094 414.094 296 392 296H56C33.906 296 16 278.094 16 256S33.906 216 56 216H392C414.094 216 432 233.906 432 256Z" />
+  ),
+  viewBox: '0 0 448 512',
+})
