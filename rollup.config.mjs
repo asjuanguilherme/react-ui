be6dd69659370492e2054be2e5fac75e879ee7fe
Juan Guilherme <asjuanguilherme@gmail.com>
2023-12-09 05:33:51 -0300

    feat: :wrench: adjust build

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -6,6 +6,7 @@ import dts from 'rollup-plugin-dts'
 import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 import postcss from 'rollup-plugin-postcss'
 import pkg from './package.json' assert { type: 'json' }
+import babel from '@rollup/plugin-babel'
 
 /** @type {import('rollup').RollupOptions[]} */
 export default [
@@ -34,11 +35,13 @@ export default [
         file: pkg.main,
         format: 'cjs',
         sourcemap: true,
+        interop: 'auto',
       },
       {
         file: pkg.module,
         format: 'esm',
         sourcemap: true,
+        interop: 'auto',
       },
     ],
     plugins: [
@@ -48,7 +51,16 @@ export default [
       typescript({
         tsconfig: './tsconfig.json',
         declaration: false,
-        declarationMap: false
+        declarationMap: false,
+      }),
+      babel({
+        babelHelpers: 'bundled',
+        exclude: ['node_modules'],
+        presets: ['@babel/preset-react', '@babel/preset-typescript'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        plugins: [
+          ['babel-plugin-styled-components', { ssr: true, displayName: true }],
+        ],
       }),
       postcss(),
       copy({
