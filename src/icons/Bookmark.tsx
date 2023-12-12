ac16762e04097ae6bef2b5108384744d1c79f6c1
Juan Guilherme <asjuanguilherme@gmail.com>
2023-12-12 16:51:09 -0300

    feat: :sparkles: new icons

---
+++ src/icons/Bookmark.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { createIconComponent } from './createIconComponent'
+
+export const BookmarkIcon = createIconComponent({
+  content: <>
+    <path d="M384 48V512L192 400L0 512V48C0 21.5 21.5 0 48 0H336C362.5 0 384 21.5 384 48Z"/>
+  </>,
+  viewBox: '0 0 384 512',
+})
+
