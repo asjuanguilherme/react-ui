0ceae462cac1b5fc2c17f95faccd3e94e60e79a8
Juan Guilherme <asjuanguilherme@gmail.com>
2023-12-14 11:38:44 -0300

    fix: :bug: popover box style + improve code quality by using mobile first pattern

--- src/components/Popover/partials/Box/styles.ts
+++ src/components/Popover/partials/Box/styles.ts
@@ -1,6 +1,6 @@
 import styled, { css } from 'styled-components'
 
-import { breakpoints, screens } from 'lib/responsiveness'
+import { screens } from 'lib/responsiveness'
 
 import { CardBase } from 'components/CardBase'
 
@@ -13,29 +13,23 @@ export const Box = styled(CardBase)<{
   $maxHeight?: number
   $maxWidth?: number
 }>`
-  @media screen and (max-width: ${breakpoints.tablet}px) {
-    width: 100%;
-    max-width: 100%;
-    position: static;
-    flex-shrink: 0;
-
-    ${({ $visible }) =>
-      $visible &&
-      css`
-        pointer-events: all;
-      `}
-  }
-
+  width: 100%;
   overflow-y: hidden;
-  position: absolute;
-  z-index: 10;
 
-  transition: ${props => props.theme.transitionDurations.default};
-  transition-property: background, border, transform, opacity, top;
+  ${({ $visible }) =>
+    $visible &&
+    css`
+      pointer-events: all;
+    `}
 
   ${screens.tablet} {
     top: 100%;
+    position: absolute;
+    z-index: 10;
     width: ${({ $maxWidth }) => ($maxWidth ? $maxWidth + 'px' : 'max-content')};
+    max-width: max-content;
+    transition: ${props => props.theme.transitionDurations.default};
+    transition-property: background, border, transform, opacity, top;
 
     ${({ $maxHeight }) =>
       $maxHeight &&
@@ -73,26 +67,35 @@ export const Box = styled(CardBase)<{
 `
 
 export const Wrapper = styled.div<{ $visible: boolean }>`
-  @media screen and (max-width: ${breakpoints.tablet}px) {
-    background: rgba(0, 0, 0, 0.3);
-    height: 100vh;
-    max-height: 100%;
-    width: 100%;
-    position: fixed;
-    left: 0;
-    top: 0;
-    z-index: ${props => props.theme.zIndex.modals};
-    padding: ${props => props.theme.container.padding};
-    display: flex;
-    align-items: flex-end;
-    transition-duration: ${props => props.theme.transitionDurations.default};
-    transition-property: opacity;
+  background: rgba(0, 0, 0, 0.3);
+  height: 100vh;
+  max-height: 100%;
+  width: 100%;
+  position: fixed;
+  left: 0;
+  top: 0;
+  z-index: ${props => props.theme.zIndex.modals};
+  padding: ${props => props.theme.container.padding};
+  display: flex;
+  align-items: flex-end;
+  transition-duration: ${props => props.theme.transitionDurations.default};
+  transition-property: opacity;
 
-    ${({ $visible }) =>
-      !$visible &&
-      css`
-        pointer-events: none;
-        opacity: 0;
-      `}
+  ${({ $visible }) =>
+    !$visible &&
+    css`
+      pointer-events: none;
+      opacity: 0;
+    `}
+
+  ${screens.tablet} {
+    background: initial;
+    height: initial;
+    max-height: initial;
+    width: initial;
+    position: static;
+    padding: 0;
+    display: block;
+    transition: initial;
   }
 `
